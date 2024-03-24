da47734ad1b3ddd27488856f80dbf30796cf40c3
vietpt21 <vietvpvbhp@gmail.com>
2024-03-24 20:33:21 +0700

    iououi

---
+++ ASP.NET.Core.MVC.NET.8/Category_CRUD_01/Category_CRUD_01/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAcess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+             modelBuilder.Entity<Category>().HasData(
+             new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1 },
+             new Category { CategoryId = 2, Name = "SciFi", DisplayOrder = 2 },
+             new Category { CategoryId = 3, Name = "Hostory", DisplayOrder = 3 }
+            );
+        }
+
+
+    }
+}
---
+++ ASP.NET.Core.MVC.NET.8/Category_CRUD_01/Category_CRUD_01/Bulky.DataAccess/Repository/RepositoryCategory.cs
@@ -0,0 +1,29 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.DataAcess.Data;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class RepositoryCategory : Repository<Category>, IRepositoryCategory
+    {
+        private readonly ApplicationDbContext _db;
+        public RepositoryCategory(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
