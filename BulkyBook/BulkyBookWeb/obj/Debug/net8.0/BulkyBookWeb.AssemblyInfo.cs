b90dc7af35b502ed5f9628615e9aa0f17eb21bb0
vietpt21 <vietvpvbhp@gmail.com>
2024-04-21 15:24:20 +0700

    ProductVM

--- BulkyBook/BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBook/BulkyBookWeb/obj/Debug/net8.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d81ead274ba22f00f648d87f31bf8f53f4397c36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69acabdb3046e48f8b8c16029cadad9a7c4622d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
