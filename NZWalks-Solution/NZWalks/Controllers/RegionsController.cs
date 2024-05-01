1e318388453337586294ca8d424ff84f912501a2
vietpt21 <vietvpvbhp@gmail.com>
2024-05-01 21:30:52 +0700

    dsfs

---
+++ NZWalks-Solution/NZWalks/Controllers/RegionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.Repositories;
+
+namespace NZWalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly IRegionRepository regionRepository;
+        public RegionsController(IRegionRepository _regionRepository)
+        {
+            this.regionRepository = _regionRepository;
+        }
+        [HttpGet]
+        //[Authorize(Roles = "Reader")]
+        public async Task<IActionResult> GetAll()
+        {
+            // Get Data From Database - Domain models
+            var regionsDomain = await regionRepository.GetAllAsync();
+
+            // Return DTOs
+            return Ok(regionsDomain);
+        }
+
+    }
+}
